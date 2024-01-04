3de48f603060612a2944c797ec8edc9884dc7a94
HAdiiiir <hader.jaamal2010@gmail.com>
2024-01-04 15:59:02 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-def print_argvs(args):
-    """Prints the number and list of arguments.
-
-    Args:
-        args: A list of arguments.
-    """
-
-    number_of_args = len(args)
-    if number_of_args == 0:
-        print("0 arguments.")
-    elif number_of_args == 1:
-        print(f"1 argument:")
-        print(f"1: {args[0]}")
-    else:
-        print(f"{number_of_args} arguments:")
-        for i in range(number_of_args):
-            print(f"{i + 1}: {args[i]}")
-
 if __name__ == "__main__":
-    print_argvs(sys.argv[1:])
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+
