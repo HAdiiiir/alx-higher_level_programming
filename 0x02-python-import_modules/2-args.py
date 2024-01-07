b74e73dbdbbb264e7b0136e038405f1f4e9503e6
HAdiiiir <hader.jaamal2010@gmail.com>
2024-01-06 21:09:40 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+import sys
+
+
+def print_argvs(args):
+
+    """Prints the number and list of arguments.
+
+    Args:
+        args: A list of arguments.
+    """
+
+    number_of_args = len(args)
+    if number_of_args == 0:
+        print("0 arguments.")
+    elif number_of_args == 1:
+        print(f"1 argument:")
+        print(f"1: {args[0]}")
+    else:
+        print(f"{number_of_args} arguments:")
+        for i in range(number_of_args):
+            print(f"{i + 1}: {args[i]}")
+
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-
-    a = 10
-    b = 5
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print_argvs(sys.argv[1:])
