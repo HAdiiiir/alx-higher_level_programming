25333422dc9acf45f185511ac34e7b69bdc9f14d
HAdiiiir <hader.jaamal2010@gmail.com>
2024-04-08 18:02:05 -0700

    a

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 
 #!/usr/bin/node
-function printArgs(argv) {
-  if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
-printArgs();
