16ad91b625e4a9b0641b0827c9445e45fb4b284a
HAdiiiir <hader.jaamal2010@gmail.com>
2024-04-09 19:55:49 -0700

    a

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 
 #!/usr/bin/node
-// Prints a specified string only if the first argument can be converted to an integer
+// prints two arguments passed to it, in the following format: “ is ”
 
-const args = process.argv;
-const number = parseInt(args[2], 10);
-if (isNaN(number)) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${number}`);
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
