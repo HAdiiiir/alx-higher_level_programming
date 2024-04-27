e7deebc21405f26b9cf69b14c19b518880522471
HAdiiiir <hader.jaamal2010@gmail.com>
2024-04-27 08:12:42 -0700

    a

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the size of the body of the response from the url
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+# Script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
