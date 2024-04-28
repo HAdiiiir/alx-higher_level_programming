c736670188ab3e14569ff6a204013ea106ad9597
HAdiiiir <hader.jaamal2010@gmail.com>
2024-04-28 14:07:23 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -16,6 +16,7 @@ def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
     res = get(url)
     print("Body response:")
     print("\t- type:", type(res))
+    print("\t- type: {}".format(str(res).__class__))
     print("\t- content:", res.text)
 
 
